1e6e017341bcd0fffad39bf56f107ac3c47caa68
numan <numan.codes@gmail.com>
2024-11-28 00:10:42 +0500

    fix issue 1

--- app/api/[shortUrl]/route.js
+++ app/api/[shortUrl]/route.js
@@ -2,9 +2,7 @@ import clientPromise from "@/lib/mongodb";
 
 export async function GET(req, { params }) {
   const { shortUrl } = params; // Accessing the dynamic URL parameter
-
   console.log("Short URL:", shortUrl); // This will log to your server console
-
   try {
     const client = await clientPromise;
     const db = client.db("url-shortner");
