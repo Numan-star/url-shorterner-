02e8d48a2251dc4747a0c592c0d1e0620999403a
numan <numan.codes@gmail.com>
2025-01-19 19:24:03 +0500

    fix issue

--- app/api/[shortUrl]/route.js
+++ app/api/[shortUrl]/route.js
@@ -1,7 +1,8 @@
 import clientPromise from "@/lib/mongodb";
 
-export async function GET(req, { params }) {
-  const { shortUrl } = params;
+export async function GET(req, context) {
+  const { shortUrl } = context.params; // Use 'context' to access 'params'
+
   try {
     const client = await clientPromise;
     const db = client.db("url-shortner");
