2525ee46d1277b760caf4664830a567160ea0a41
numan <numan.codes@gmail.com>
2025-01-19 20:53:35 +0500

    fix issue 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["oracledb"],
-
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = [...(config.externals || []), "oracledb"];
