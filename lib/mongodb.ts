160f96fd7f4a23377f56f25f12f4dbef2d4effff
“Numan-star” <5125.2019.gct@gmail.com>
2025-01-20 23:18:20 +0500

    fix issue for db 3

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -5,20 +5,20 @@ if (!uri) {
   throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
-let client;
-let clientPromise;
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, {
-      serverSelectionTimeoutMS: 5000, // Shortened timeout for quicker connection failures
+      serverSelectionTimeoutMS: 60000,
     });
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   client = new MongoClient(uri, {
-    serverSelectionTimeoutMS: 5000, // Shortened timeout for quicker connection failures
+    serverSelectionTimeoutMS: 60000,
   });
   clientPromise = client.connect();
 }
