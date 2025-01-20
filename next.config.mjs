8d4273b92bcbc593ec17113f9e3b3b13d391bdc1
“Numan-star” <5125.2019.gct@gmail.com>
2025-01-20 23:07:56 +0500

    fix issue for db 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: ["oracledb"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), "oracledb"];
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
